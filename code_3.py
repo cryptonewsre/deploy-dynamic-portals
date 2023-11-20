d5ac850c9f8c63ab376aff97b5a97d08c932de79
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-11-21 00:28:11 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    data = generate_random_data()
+    main()
+
+    return data
+if __name__ == "__main__":
+
+
+    for item in data:
+        print(f"Random Number: {item}")
+import random    for (let i = 0; i < 10; i++) {
+
+
+    const data = [];
+    }
+        console.log("Random Number: " + item);
+    return data;
+main();
+    data.forEach(item => {
+}
+    });
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    const data = generateRandomData();
+function generateRandomData() {
